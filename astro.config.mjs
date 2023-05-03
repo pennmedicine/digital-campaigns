568b14e30f009af118105fe3da8d95ed6a31a3e8
Scott Helm <scott.helm@uphs.upenn.edu>
2023-05-03 11:57:48 -0400

    First draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    build: {
+        format: 'directory',
+        assets: '-',
+    },
+    vite: {
+        build: {
+            rollupOptions: {
+                output: {
+                    assetFileNames: `-/media/Files/digitalcampaigns/assets/index[extname]`
+                }
+            }
+        }
+    }
+});
